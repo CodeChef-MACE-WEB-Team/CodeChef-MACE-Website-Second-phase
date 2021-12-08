b3ed370cf3e54bb245545bb99ff4f12e2be42991
sourav <souravak211@gmail.com>
2021-12-08 23:37:11 +0530

    4

--- load.js
+++ load.js
@@ -1,4 +1,18 @@
-var preload = document.getElementById('preloader');
-        function loaded(){
-            preload.style.display = 'none';
-        }
+// var preload = document.getElementById('preloader');
+//         function loaded(){
+//             preload.style.display = 'none';
+//         }
+function preloaderFadeOutInit() {
+    $('.preloader').fadeOut('slow');
+    $('body').attr('id', '');
+}
+// Window load function
+jQuery(window).on('load', function() {
+    (function($) {
+        preloaderFadeOutInit();
+    })(jQuery);
+});
+
+function send() {
+    alert("Mail sent");
+}
