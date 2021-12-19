527bc6d453aa0034bd144dbbf7b8ad86c1d12154
sourav <souravak211@gmail.com>
2021-12-20 02:09:38 +0530

    Feature update : time, visitor count device width

--- stuffs/social-counter.js
+++ stuffs/social-counter.js
@@ -1,16 +1,24 @@
-const counters = document.querySelectorAll('.visitor-counter');
-for (let n of counters) {
-    const updateCount = () => {
-        const target = +n.getAttribute('data-target');
-        const count = +n.innerText;
-        const speed = 99999;
-        const inc = target / speed;
-        if (count < target) {
-            n.innerText = Math.ceil(count + inc);
-            setTimeout(updateCount, 1);
-        } else {
-            n.innerText = target;
-        }
-    }
-    updateCount();
-}
+$.fn.jQuerySimpleCounter = function(options) {
+    var settings = $.extend({
+        start: 0,
+        end: 1000,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({ count: settings.start }).animate({ count: settings.end }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount + '+');
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#number1').jQuerySimpleCounter({ end: 1300, duration: 10000 });
